b34e310028106dbe9759cafbfc79fdc30e55a6de
Chukwuedu Usi <usichukwuedu@yahoo.com>
2021-05-14 15:34:36 -0400

    voting_system

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2018 Brought To You By<a href="https://dicefdigitalestate.com"> Dicef Digital Estate</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
