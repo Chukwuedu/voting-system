6114a33caba436eaa5772fc276c6ed82afb48a1d
Chukwuedu Usi <usichukwuedu@yahoo.com>
2021-05-14 15:36:34 -0400

    Update conn.php

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('localhost', 'voting-system', 'Voting56=', 'voting-system');
+    $conn = new mysqli('localhost', 'root', '', 'voting-system');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
